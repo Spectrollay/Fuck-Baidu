8c17bd2a8c1743286380f6d5064ec95f19469820
Spectrollay <2819288472@qq.com>
2024-05-24 16:27:02 +0800

    修复谷歌/雅虎搜索引擎不会正确过滤的问题

--- user.js
+++ user.js
@@ -1,28 +1,38 @@
-//fuck baidu
+//fuck Baidu
 // ==UserScript==
-// @name         Fuck baidu
+// @name         Fuck Baidu
 // @namespace    http://tampermonkey.net/
-// @version      1.5
-// @description  屏蔽与百度相关的搜索结果Filter out all search results related to "baidu.com" from search engines.
+// @version      1.6
+/* @description  屏蔽搜索引擎中来自百度的搜索结果\n
+Filter out all search results from "baidu.com" for some search engines.
+*/
 // @author       Hijack_Nick & Spectrollay
 // @match        *://*.google.com/*
 // @match        *://*.bing.com/*
-// @match        *://search.yahoo.com/search*
+// @match        *://*.yahoo.com/*
 // @grant        none
 // @license      MIT
 // ==/UserScript==
  
 (function() {
     'use strict';
- 
+
     const filterKeywords = ['baidu', '百度'];
- 
+
     function filterResults() {
-        const results = document.querySelectorAll('.b_algo');
- 
+        let results = [];
+
+        if (location.hostname.includes('google.com')) {
+            results = document.querySelectorAll('.g');
+        } else if (location.hostname.includes('bing.com')) {
+            results = document.querySelectorAll('.b_algo');
+        } else if (location.hostname.includes('yahoo.com')) {
+            results = document.querySelectorAll('.dd.algo');
+        }
+
         results.forEach(result => {
             const resultText = result.innerText.toLowerCase();
- 
+
             filterKeywords.forEach(keyword => {
                 if (resultText.includes(keyword)) {
                     result.style.display = 'none';
@@ -30,20 +40,20 @@
             });
         });
     }
- 
+
     filterResults();
- 
-    const searchInput = document.querySelector('#sb_form_q');
+
+    const searchInput = document.querySelector('input[name="q"], input[name="p"], #sb_form_q');
     if (searchInput) {
         searchInput.addEventListener('input', () => {
-            const results = document.querySelectorAll('.b_algo');
+            const results = document.querySelectorAll('.g, .b_algo, .dd.algo');
             results.forEach(result => {
                 result.style.display = '';
             });
             filterResults();
         });
- 
-        const searchForm = document.querySelector('#sb_form');
+
+        const searchForm = searchInput.closest('form');
         if (searchForm) {
             searchForm.addEventListener('submit', (event) => {
                 event.preventDefault();
@@ -51,7 +61,7 @@
             });
         }
     }
- 
+
     const observer = new MutationObserver((mutations) => {
         mutations.forEach((mutation) => {
             if (mutation.addedNodes.length || mutation.removedNodes.length) {
@@ -59,7 +69,7 @@
             }
         });
     });
- 
+
     observer.observe(document.body, { childList: true, subtree: true });
- 
+
 })();
